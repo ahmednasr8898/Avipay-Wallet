ca76f3cf1f3651a89962a848b2df90104b2ed8e7
ahmednasr <ahmed.nasr.saieed@gmail.com>
2025-11-04 20:34:14 +0200

    Initial Commit

---
+++ Avipay-Wallet/Avipay_WalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  Avipay_WalletApp.swift
+//  Avipay-Wallet
+//
+//  Created by Ahmed Nasr on 04/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct Avipay_WalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
