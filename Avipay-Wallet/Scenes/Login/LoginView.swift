62179a00a6996da2f84404b05a7bf38baad25de5
Ahnmed Khalil <ahmed.nasr.saieed@gmail.com>
2025-11-17 17:12:56 +0200

    Update LoginView.swift
    - Complete login page UI

--- Avipay-Wallet/Scenes/Login/LoginView.swift
+++ Avipay-Wallet/Scenes/Login/LoginView.swift
@@ -11,6 +11,7 @@ struct LoginView: View {
     
     @State private var phone: String = ""
     @State private var password: String = ""
+    @State private var isSavePasswordSelected = false
     
     var body: some View {
         VStack(spacing: 0) {
@@ -21,11 +22,71 @@ struct LoginView: View {
             VStack(spacing: 28) {
                 
                 AvipayTextFieldView(title: "Phone", type: .phone, text: $phone)
-                
                 AvipayTextFieldView(title: "Password", type: .password, text: $password)
             }
-            
             .padding([.horizontal, .top], 24)
+            
+            HStack {
+                HStack(spacing: 4) {
+                    Button {
+                        isSavePasswordSelected.toggle()
+                    } label: {
+                        Image(systemName: isSavePasswordSelected ? "checkmark.square.fill" : "app")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 16, height: 16)
+                            .foregroundStyle(Color.primaryAvipay)
+                    }
+                    .background(
+                        RoundedRectangle(cornerRadius: 12)
+                            .fill(Color.surfaceAvipay)
+                    )
+                    
+                    Text("Save password")
+                        .font(.caption2)
+                        .foregroundStyle(Color.onSurfaceAvipay)
+                }
+                Spacer()
+                
+                Button {
+                    // Tapped on forgot password.
+                } label: {
+                    Text("Forgot password?")
+                        .font(.caption2)
+                        .foregroundStyle(Color.primaryAvipay)
+                }
+
+            }
+            .padding(.top, 10)
+            .padding(.horizontal, 24)
+            
+            
+            Button(action: {
+                // Tapped on login
+            }, label: {
+                Text(" Login")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+            })
+            .buttonStyle(.normal)
+            .padding(.top, 40)
+            .padding(.horizontal, 24)
+            
+            HStack(spacing: 4) {
+                Text("Don’t have account?")
+                    .font(.bodyText)
+                    .foregroundStyle(Color.onSurfaceAvipay)
+
+                
+                Button {
+                    // Tapped on sign up
+                } label: {
+                    Text("Sign Up")
+                        .font(.bodyText)
+                        .foregroundStyle(Color.primaryAvipay)
+                }
+            }
+            .padding(.top, 16)
             Spacer()
         }
     }
