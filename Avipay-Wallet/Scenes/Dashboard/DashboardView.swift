121c3d52f317ad5b030b57814aa31b08c0b06e47
Ahnmed Khalil <ahmed.nasr.saieed@gmail.com>
2026-01-14 14:34:41 +0200

    Create DashboardView

--- Avipay-Wallet/Scenes/Dashboard/DashboardView.swift
+++ Avipay-Wallet/Scenes/Dashboard/DashboardView.swift
@@ -2,14 +2,14 @@
 //  DashboardView.swift
 //  Avipay-Wallet
 //
-//  Created by Ahmed Khalil, Vodafone on 05/01/2026.
+//  Created by Ahmed Khalil, Vodafone on 14/01/2026.
 //
 
 import SwiftUI
 
 struct DashboardView: View {
     var body: some View {
-        Text("Dashboard")
+        Text("Dashboard View")
     }
 }
 
